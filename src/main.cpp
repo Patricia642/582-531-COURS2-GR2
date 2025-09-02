ed0dea058705d84da060a7d3d994c658c592dfdf
Nassif <2142168@cmontmorency.qc.ca>
2025-09-02 16:46:44 -0400

    Initialisation

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#define MA_BROCHE_ANGLE 32
+#include <FastLED.h>
+CRGB monPixel;
+
+void setup() {
+  Serial.begin(115200);
+  FastLED.addLeds<WS2812, 27, GRB>(&monPixel, 1);
+}
+
+void loop() {
+  int maLectureAnalogique;
+  maLectureAnalogique = analogRead(MA_BROCHE_ANGLE);
+
+  Serial.println(maLectureAnalogique);
+  delay(100);
+
+  if (maLectureAnalogique >= 2047)
+  {
+    monPixel = CRGB(253, 75, 122);
+  FastLED.show();
+  } else if (maLectureAnalogique < 2047) {
+    FastLED.clear(true);
+  }
+  
+}
+
+
+
